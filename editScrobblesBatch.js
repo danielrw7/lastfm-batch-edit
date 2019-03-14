755ac87bf872665895ddcbd4e7d52109e6f1d57a
Daniel Wilson <danielrw7@gmail.com>
2019-03-14 19:28:33 -0400

    Replace hardcoded sleep with waitForPopup method; Add filtering out scrobbles that match new attributes

--- editScrobblesBatch.js
+++ editScrobblesBatch.js
@@ -2,20 +2,42 @@ const $ = jQuery
 async function sleep(ms) {
     return new Promise((r) => setTimeout(r, ms))
 }
+const waitForPopupMaxTries = 100
+async function waitForPopup(open) {
+    for (let tries = 0; tries < waitForPopupMaxTries; tries++) {
+        await sleep(100)
+        if (open === !!($('.modal-dialog').length)) {
+            break
+        }
+    }
+}
 async function editScrobble(form, newAttributes = {}) {
     const $form = $(form)
     $form.find('button').click()
-    await sleep(2000)
+    await waitForPopup(true)
     Object.entries(newAttributes).forEach(([key, value]) => {
         $('#id_' + key).val(value)
+        $form.find(`[name="${key}"]`).val(value)
     })
     $('.modal-body [type="submit"]').click()
+    await waitForPopup(false)
+}
+function scrobbleHasChange(form, newAttributes = {}) {
+    const $form = $(form)
+    for (let attribute of Object.entries(newAttributes)) {
+        const [key, value] = attribute
+        if ($form.find(`[name="${key}"]`).val() !== value) {
+            return true
+        }
+    }
+    return false
 }
 async function editScrobblesBatch(newAttributes = {}) {
-    const forms = $('[action*="/library/edit"]').toArray()
+    const forms = $('[action*="/library/edit"]').toArray().filter((form) => {
+        return scrobbleHasChange(form, newAttributes)
+    })
     for (let form of forms) {
         await editScrobble(form, newAttributes)
-        await sleep(1000)
     }
 }
 
